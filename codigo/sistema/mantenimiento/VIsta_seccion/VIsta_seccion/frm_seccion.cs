11e0a750eb578de1b220606761407b0903263573
Ruben Armando Lopez Luch <rlopezl24@miumg.edu.gt>
2026-05-28 17:19:40 -0600

    Me negaba subir mi proyecto, probe intentar de nuevo subir, no arrelge mi codigo, asi como se mostro en mi video, el codigo se mantiene igual con el problema con navegador

---
+++ codigo/sistema/mantenimiento/VIsta_seccion/VIsta_seccion/frm_seccion.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VIsta_seccion
+{
+    public partial class frm_seccion : Form
+    {
+        public frm_seccion()
+        {
+            InitializeComponent();
+
+            //parametros para navegador
+            Capa_Controlador_Navegador.Cls_ConfiguracionDataGridView config = new Capa_Controlador_Navegador.Cls_ConfiguracionDataGridView
+            {
+                Ancho = 1100,
+                Alto = 200,
+                PosX = 10,
+                PosY = 300,
+                ColorFondo = Color.AliceBlue,
+                TipoScrollBars = ScrollBars.Both,
+                Nombre = "dgv_empleados"
+            };
+
+            string[] columnas = {
+     "secciones",
+     "codigo_seccion",
+     "nombre_seccion",
+     "estatus_seccion"
+
+
+ };
+
+            string[] sEtiquetas = {
+    "Codigo: ",
+     "Nombre: ",
+     "Estatus: ",
+
+ };
+
+
+
+            int id_aplicacion = 711;
+            int id_modulo = 5;
+
+            navegador1.IPkId_Aplicacion = id_aplicacion;
+            navegador1.IPkId_Modulo = id_modulo;
+            navegador1.configurarDataGridView(config);
+            navegador1.SNombreTabla = columnas[0];
+            navegador1.SAlias = columnas;
+            navegador1.SEtiquetas = sEtiquetas;
+            navegador1.mostrarDatos();
+        }
+    }
+}
